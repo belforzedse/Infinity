d2d2e2c797dc4ad391f4b73f99dfb8b043ea6a15
bel4 <yazdannaderi@hotmail.com>
2025-09-09 14:30:03 +0330

    feat: add user container layout

--- frontend/src/app/(user)/cart/page.tsx
+++ frontend/src/app/(user)/cart/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 import ShoppingCart from "@/components/ShoppingCart";
+import UserContainer from "@/components/layout/UserContainer";
 
 export default function CartPage() {
   return (
-    // TODO: change contaianer to handle padding and delete padding from main
-    <div
-      className="container mx-auto flex min-h-[60vh] gap-10 bg-white lg:p-0"
+    <UserContainer
+      className="flex min-h-[60vh] gap-10 bg-white"
       dir="rtl"
     >
       <main className="flex flex-1 flex-col gap-3 overflow-y-auto">
         <ShoppingCart />
       </main>
-    </div>
+    </UserContainer>
   );
 }
