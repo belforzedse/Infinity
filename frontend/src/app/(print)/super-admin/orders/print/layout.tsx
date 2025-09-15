faa47ca6b0fa670947f08ace2d629d10c4d7bac8
bel4 <yazdannaderi@hotmail.com>
2025-09-15 16:55:34 +0330

    even better

--- frontend/src/app/(print)/super-admin/orders/print/layout.tsx
+++ frontend/src/app/(print)/super-admin/orders/print/layout.tsx
@@ -8,8 +8,8 @@ export default function PrintLayout({
 }) {
   return (
     // Full-viewport overlay to hide the rest of the app UI while on print routes
-    <div className="print-layout-overlay">
-      <div className="print-wrapper">{children}</div>
+    <div>
+      <div>{children}</div>
     </div>
   );
 }
