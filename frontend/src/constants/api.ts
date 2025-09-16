b71455ea34376df9b422034cef541c3ed5cce5ce
bel4 <yazdannaderi@hotmail.com>
2025-09-16 18:51:11 +0330

    e

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -4,15 +4,19 @@
  */
 
 // Base URLs
-const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+const apiBaseUrl =
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  "https://api.infinity.rgbgroup.ir/api";
 export const API_BASE_URL = apiBaseUrl;
 
 const strapiToken = process.env.NEXT_PUBLIC_STRAPI_TOKEN;
 
+export const STRAPI_TOKEN =
+  strapiToken ||
+  "STRAPI_API_TOKEN";
 
-export const STRAPI_TOKEN = strapiToken;
-
-const imageBaseUrl = process.env.NEXT_PUBLIC_IMAGE_BASE_URL;
+const imageBaseUrl =
+  process.env.NEXT_PUBLIC_IMAGE_BASE_URL || "https://api.infinity.rgbgroup.ir";
 
 export const IMAGE_BASE_URL = imageBaseUrl;
 // API Versions
