8a96aab10df11a4de1c19915cfe57ab9451694fb
bel4 <yazdannaderi@hotmail.com>
2025-09-17 16:54:27 +0330

    f

--- frontend/src/constants/productFilters.ts
+++ frontend/src/constants/productFilters.ts
@@ -1,7 +1,7 @@
 // Product title filters used throughout the app
-// Matches titles that contain کیف, کفش, صندل, or کتونی (case-insensitive, substring)
+// Matches titles that contain کیف, کفش, صندل, کتونی, or ونس (case-insensitive, substring)
 export const PRODUCT_TITLE_FILTER =
-  "&filters[$or][0][Title][$containsi]=کیف&filters[$or][1][Title][$containsi]=کفش&filters[$or][2][Title][$containsi]=صندل&filters[$or][3][Title][$containsi]=کتونی";
+  "&filters[$or][0][Title][$containsi]=کیف&filters[$or][1][Title][$containsi]=کفش&filters[$or][2][Title][$containsi]=صندل&filters[$or][3][Title][$containsi]=کتونی&filters[$or][4][Title][$containsi]=ونس";
 
 /**
  * Helper to append the product title filter to an endpoint safely.
